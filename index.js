b617f46f67b3584f7659a63c6a923512f57fb374
Park jiwon <pannchat@likelion.org>
2021-06-07 19:23:13 +0900

    service-worker

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if ("serviceWorker" in navigator) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker.register("/service-worker.js");
+        });
+    }
 function calc(){
     var s = 100;    // scale
     // 화면에 보여지는 값들
