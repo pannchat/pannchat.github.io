a0387be1820255f48c9f82464b5b5561db59bd1f
pannchat <pannchat@likelion.org>
2021-06-30 00:59:10 +0900

    modify:index.js

--- index.js
+++ index.js
@@ -4,39 +4,29 @@ if ("serviceWorker" in navigator) {
             navigator.serviceWorker.register("/service-worker.js");
         });
     }
-// Initialize deferredPrompt for use later to show browser install prompt.
+// 크롬에서만 pwa 설치버튼 보여주기. 
+if( (navigator.userAgent).indexOf("Chrome") > -1){
+            document.getElementById("buttonInstall").style.display = 'block';
+}
 let deferredPrompt;
 const divInstall = document.getElementsByClassName('divInstall')[0];
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('👍', 'beforeinstallprompt', event);
-    // Stash the event so it can be triggered later.
     window.deferredPrompt = event;
-    // Remove the 'hidden' class from the install button container
     divInstall.classList.toggle('hidden', false);
   });
 
 const buttonInstall = document.getElementById('buttonInstall');
 buttonInstall.addEventListener('click', async () => {
-    console.log('👍', 'butInstall-clicked');
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
-      // The deferred prompt isn't available.
       return;
     }
-    // Show the install prompt.
     promptEvent.prompt();
-    // Log the result
     const result = await promptEvent.userChoice;
-    console.log('👍', 'userChoice', result);
-    // Reset the deferred prompt variable, since
-    // prompt() can only be called once.
     window.deferredPrompt = null;
-    // Hide the install button.
     divInstall.classList.toggle('hidden', true);
   });
   window.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
-    // Clear the deferredPrompt so it can be garbage collected
     window.deferredPrompt = null;
   });
 
